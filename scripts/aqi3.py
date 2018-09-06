4ba27a96bb0f9f2251a9c671851ecb10f2ec27d7
Tiago Conceição <Tiago_caza@hotmail.com>
2018-09-06 19:55:46 +0100

    Fix error with logcmd

--- scripts/aqi3.py
+++ scripts/aqi3.py
@@ -6,23 +6,24 @@ import time
 from sds011 import SDS011
 
 # CONFIGURATION
-SERIALPORT = "/dev/ttyUSB0" # USB port where SDS011 is
+SERIALPORT = "/dev/ttyUSB0"  # USB port where SDS011 is
 # This will influence the accuracy and speed of readings, keep a good balance
 # Less reads = Less precision and fast
 # More reads = More precision and slow
 # After measurements the sensor, laser and fan will be turn off for 'UPDATE_FREQUENCY' time, this will increase the lifespan of the sensor.
-READINGS = 5                        # Number of readings, this will not perform an AVG, only the last read will be used as value.
-SLEEP_BEFORE_FIRST_READ = 15        # Time to wait in seconds after sensor awake and before the first read. Give sometime for the sensor stabilize
-SLEEP_BETWEEN_READS = 2             # Time to sleep in seconds between each read, total read time will be READINGS x SLEEP_BETWEEN_READS.
-UPDATE_FREQUENCY = 60               # Update frequency in seconds, new measurements after that time.
+READINGS = 5  # Number of readings, this will not perform an AVG, only the last read will be used as value.
+SLEEP_BEFORE_FIRST_READ = 15  # Time to wait in seconds after sensor awake and before the first read. Give sometime for the sensor stabilize
+SLEEP_BETWEEN_READS = 2  # Time to sleep in seconds between each read, total read time will be READINGS x SLEEP_BETWEEN_READS.
+UPDATE_FREQUENCY = 60  # Update frequency in seconds, new measurements after that time.
 # If UPDATE_FREQUENCY = 0, the sensor will never turn off, this will wear your sensor much faster.
 # (according to the manufacturer, the lifespan totals approximately 8000 hours).
-STORED_READ_NUM = 100               # Maximum number of readings to plot or store, when max is reached, the oldest read will be removed.
+STORED_READ_NUM = 100  # Maximum number of readings to plot or store, when max is reached, the oldest read will be removed.
 
 
 def logcmd(text):
     if 'debug' in sys.argv:
         print(text)
+
 
 # Don't change
 DATA_FILE = '/var/www/html/assets/aqi.json'
@@ -41,7 +42,7 @@ if __name__ == "__main__":
                 time.sleep(SLEEP_BETWEEN_READS)
                 values = sensor.query()
                 if values is not None and len(values) == 2:
-                    logcmd(str(t + 1) + "# PM2.5: ", values[0], ", PM10: ", values[1])
+                    logcmd(str(t + 1) + "# PM2.5: " + str(values[0]) + ", PM10: " + str(values[1]))
 
             if values is not None and len(values) == 2:
                 # open stored data
